d644308598f13598abf3af4b776c57fd2af4274e
lenalemo8 <yellowscarf8@gmail.com>
2025-05-22 23:59:47 +0300

    project initialized, .gitignore added

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+      baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
+      video: true,
+      projectId: "7tyfmt",
+    },
+});
